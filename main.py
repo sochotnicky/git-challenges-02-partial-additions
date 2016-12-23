22644f3e72184509abaed49f169c3b5f5646afb9
Stanislav Ochotnicky <sochotnicky@redhat.com>
2016-12-23 11:52:14 +0100

    RESET THIS COMMIT BEFORE STARTING!
    Run "git reset --mixed HEAD~1"

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ import sys
 def my_method():
     pass
 
+def square(num):
+    return num**2
+
+def print_square():
+    num = int(raw_input("Input number: "))
+    print("Square: %d" % square(num))
+
 def my_second_method():
     print("This will not work for a long time")
 
